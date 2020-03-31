edf731f6c200408d40ed5987ca881dc7fdc5cb5e
alevall0 <37554522+alevall0@users.noreply.github.com>
2020-03-30 22:18:41 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RevisaSumaEnColeccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 7;
+            bool flag = false;
+            flag = VerificaSuma(suma);
+            Console.WriteLine("La suma existe en el arreglo ? " +  flag);
+        }
+
+        static bool VerificaSuma(int suma)
+        {
+            int[] arrNumeros = { 1, 4, 5, 2 };
+            HashSet<int> hSet = new HashSet<int>();
+            foreach (var n in arrNumeros)
+            {
+                if (hSet.Contains(n))
+                    return true;
+                hSet.Add(suma - n);
+            }
+            return false;
+        }
+    }
+
+}
